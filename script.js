4a9971eefa41d6b1a92b6f2d7e50a506bd5e8808
Dmitry Korochkin <abramslost@gmail.com>
2022-05-16 16:15:51 +0300

    Выполнены правки по уроку

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let title = 'glo-academy';
-let screens = "Простые, Сложные, Интерактивные";
-let screensPrice = 400;
-let rollback = 60;
-let fullPrice = 15000;
-let adaptive = true; 
+const title = 'glo-academy';
+const screens = "Простые, Сложные, Интерактивные";
+const screensPrice = 400;
+const rollback = 60;
+const fullPrice = 15000;
+const adaptive = true; 
 
 
-console.log('title', 'fullPrice', 'adaptive');
+console.log(typeof fullPrice);
+console.log(typeof title);
+console.log(typeof adaptive);
 console.log(screens.length);
 console.log("Стоимость верстки экранов " + screensPrice + " руб");
 console.log("Стоимость разработки сайта " + fullPrice + " руб");
-console.log(screens.toLowerCase());
-console.log(screens.split(","));
-console.log(fullPrice/(rollback/100));
+
+console.log(screens.toLowerCase().split(', '));
+
+
+
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback/100)) + " руб");
+
