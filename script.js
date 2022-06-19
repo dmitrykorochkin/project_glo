fcf055e77108251fa7a03fe8020ce76514231984
Dmitry Korochkin <abramslost@gmail.com>
2022-06-19 12:01:39 +0300

    выполненно дз урока 16

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class First {
+  constructor() {}
+    hello() {
+      console.log(`Привет я метод родителя!`);
+    }
+  
+}
+
+
+class Second extends First {
+  constructor() {
+   super();
+  }
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const nameClass =new Second();
+nameClass.hello();
